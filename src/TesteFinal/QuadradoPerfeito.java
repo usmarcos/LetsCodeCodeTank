a4dd6355e8dd6c6feded2cdc01d9c590bf2e8ca8
Marcos V <67884162+usmarcos@users.noreply.github.com>
2023-01-23 16:14:54 -0300

    Update QuadradoPerfeito.java

--- src/TesteFinal/QuadradoPerfeito.java
+++ src/TesteFinal/QuadradoPerfeito.java
@@ -3,72 +3,46 @@ package TesteFinal;
 import java.util.Random;
 import java.util.Scanner;
 
-class Scratch {
+public class QuadradoPerfeito {
+    /**
+     * Escreva um algoritmo que calcule se o quadrado pode ser considerado como perfeito. Para isso deve ser capturado o tamanho do quadrado a partir de input do usuário.
+     */
     public static void main(String[] args) {
-//        int[][] matriz = { {20, 6, 7, 17},
-//                {9, 15, 14, 12},
-//                {13, 11, 10, 16},
-//                {8, 18, 19, 5} };
-        int [][] matriz = montarEPreencherQuadrado();
 
-        int soma = 0;
-        boolean isPerfeito = true;
+        //int[][] matriz = montarEPreencherQuadrado();
+        int[][] matriz = {{20, 6, 7, 17},
+                {9, 15, 14, 12},
+                {13, 11, 10, 16},
+                {8, 18, 19, 5}};
 
-        //calcula a soma da primeira linha para comparar com as outras
+        // verifica linhas
+        int totalLinha = 0;
+        int totalColuna = 0;
+        int somaDiagonalPrincipal = 0;
+        int somaDiagonalSecundaria = 0;
+        int controleDiagonalPrincipal = 0;
+        int controleDiagonalSecundaria = matriz.length - 1;
+
+
         for (int i = 0; i < matriz.length; i++) {
-            soma += matriz[0][i];
+            //linha e coluna, invertendo i e j para mudar as posições de leitura
+            for (int j = 0; j < matriz.length; j++) {
+                totalLinha += matriz[j][i];
+                totalColuna += matriz[i][j];
+                somaDiagonalPrincipal += matriz[controleDiagonalPrincipal][i];
+                somaDiagonalSecundaria += matriz[i][controleDiagonalSecundaria];
+            }
+            controleDiagonalPrincipal++;
+            controleDiagonalSecundaria--;
         }
 
-        //compara a soma das linhas
-        for (int i = 0; i < matriz.length; i++) {
-            int linhaSoma = 0;
-            for (int j = 0; j < matriz[i].length; j++) {
-                linhaSoma += matriz[i][j];
-            }
-            if (linhaSoma != soma) {
-                isPerfeito = false;
-                break;
-            }
+        //verificando se é um quadrado perfeito
+        if (totalLinha == totalColuna && somaDiagonalPrincipal == somaDiagonalSecundaria){
+            System.out.println("Quadrado perfeito");
+        }else{
+            System.out.println("Não é um quadrado perfeito");
         }
 
-        //compara a soma das colunas
-        if (isPerfeito) {
-            for (int i = 0; i < matriz.length; i++) {
-                int colunaSoma = 0;
-                for (int j = 0; j < matriz[i].length; j++) {
-                    colunaSoma += matriz[j][i];
-                }
-                if (colunaSoma != soma) {
-                    isPerfeito = false;
-                    break;
-                }
-            }
-        }
-
-        //compara a soma das diagonais
-        if (isPerfeito) {
-            int diagonalPrincipal = 0;
-            int diagonalSecundaria = 0;
-            for (int i = 0; i < matriz.length; i++) {
-                for (int j = 0; j < matriz[i].length; j++) {
-                    if (i == j) {
-                        diagonalPrincipal += matriz[i][j];
-                    }
-                    if (i + j == matriz.length - 1) {
-                        diagonalSecundaria += matriz[i][j];
-                    }
-                }
-            }
-            if (diagonalPrincipal != soma || diagonalSecundaria != soma) {
-                isPerfeito = false;
-            }
-        }
-
-        if (isPerfeito) {
-            System.out.println("O quadrado é perfeito!");
-        } else {
-            System.out.println("O quadrado não é perfeito.");
-        }
     }
 
     public static int[][] montarEPreencherQuadrado() {
